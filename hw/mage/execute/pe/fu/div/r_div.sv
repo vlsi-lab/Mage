0ed7528580f58cb153f2c23ac6ea0ea241c2d696
alessionaclerio22 <alessionaclerio22@gmail.com>
2025-05-15 15:28:38 +0200

    Fix divider
    Solve bug on zero input operands. Allow for signed division.

--- hw/mage/execute/pe/fu/div/r_div.sv
+++ hw/mage/execute/pe/fu/div/r_div.sv
@@ -20,6 +20,7 @@ module r_div
     output logic              valid_o
 );
 
+  // divider state
   typedef enum logic [1:0] {
     IDLE,
     EXEC,
@@ -31,14 +32,34 @@ module r_div
   logic [N_BITS-1:0] r_out_stage_out_reg;
   logic [N_BITS-1:0] r_in_stage;
   logic [$clog2(N_RADIX)-1:0] q_out_stage;
+  logic [N_BITS-1:0] q_final;
+  logic [N_BITS-1:0] r_final;
+  logic [N_BITS-1:0] q_final_inv;
+  logic [N_BITS-1:0] r_final_inv;
   logic [N_BITS-1:0] q_out;
   logic [N_BITS-1:0] n_in_stage;
   logic [N_BITS-1:0] n_in;
+
   // fsm-realted signals
   logic [$clog2(N_DIV_STAGE)-1:0] cnt;
   div_fsm_t div_state_n;
   div_fsm_t div_state_c;
 
+  // sign signals
+  logic n_is_neg;
+  logic d_is_neg;
+  logic n_is_zero;
+  logic d_is_zero;
+
+  // dividend and divisor sign
+  assign n_is_neg = n_i[N_BITS-1];
+  assign d_is_neg = d_i[N_BITS-1];
+
+  // zero check
+  assign n_is_zero = (n_i == '0);
+  assign d_is_zero = (d_i == '0);
+
+  // radix-configurable divider core
   r_div_stage r_div_stage_inst (
       .n_i(n_in[N_BITS-1:N_BITS-$clog2(N_RADIX)]),
       .d_i(d_i),
@@ -47,6 +68,7 @@ module r_div
       .q_o(q_out_stage)
   );
 
+  // state transition
   always_ff @(posedge clk_i, negedge rst_n_i) begin
     if (!rst_n_i) begin
       div_state_c <= IDLE;
@@ -55,6 +77,7 @@ module r_div
     end
   end
 
+  // counter with enable
   always_ff @(posedge clk_i, negedge rst_n_i) begin
     if (!rst_n_i) begin
       cnt <= '0;
@@ -103,6 +126,7 @@ module r_div
     endcase
   end
 
+  // output register for divider core
   always_ff @(posedge clk_i, negedge rst_n_i) begin
     if (!rst_n_i) begin
       r_out_stage_out_reg <= '0;
@@ -110,9 +134,6 @@ module r_div
       r_out_stage_out_reg <= r_out_stage_in_reg;
     end
   end
-
-  assign valid_o = (div_state_c == FINISH);
-  assign r_o = r_out_stage_in_reg;
 
   always_ff @(posedge clk_i, negedge rst_n_i) begin
     if (!rst_n_i) begin
@@ -126,9 +147,7 @@ module r_div
     end
   end
 
-  always_comb begin
-    q_o = {q_out[N_BITS-1-$clog2(N_RADIX):0], q_out_stage[$clog2(N_RADIX)-1:0]};
-  end
+  
 
   always_ff @(posedge clk_i, negedge rst_n_i) begin
     if (!rst_n_i) begin
@@ -143,5 +162,32 @@ module r_div
   end
 
   assign n_in = (cnt == 0) ? n_i : n_in_stage;
+  assign q_final = {q_out[N_BITS-1-$clog2(N_RADIX):0], q_out_stage[$clog2(N_RADIX)-1:0]};
+  assign r_final = r_out_stage_in_reg;
+  assign q_final_inv = ~q_final + 1;
+  assign r_final_inv = ~r_final + 1; 
+
+  always_comb begin
+    valid_o = (div_state_c == FINISH);
+
+    if (n_is_zero || d_is_zero) begin
+      r_o = '0;
+      q_o = '0;
+    end else begin
+      if (n_is_neg && !d_is_neg) begin
+        q_o = q_final_inv;
+        r_o = r_final_inv;
+      end else if (!n_is_neg && d_is_neg) begin
+        q_o = q_final_inv;
+        r_o = r_final;
+      end else if (n_is_neg && d_is_neg) begin
+        q_o = q_final;
+        r_o = r_final_inv;
+      end else begin
+        q_o = q_final;
+        r_o = r_final;
+      end
+    end
+  end
 
 endmodule
